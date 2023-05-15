981e242956f52484121ef15182ff9373976883f8
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-15 17:59:18 -0300

    botoes autenticados

--- frontend/src/components/layouts/NavigationBar.js
+++ frontend/src/components/layouts/NavigationBar.js
@@ -3,8 +3,11 @@ import { useContext, React } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import Logo from "../../assets/img/Logo.png";
+import { Context } from "../../context/UserContext";
 
 const NavigationBar = () => {
+  const { authenticated, logout } = useContext(Context);
+
   return (
     <Navbar bg="light" expand="sm">
       <Navbar.Brand href="/">
@@ -23,19 +26,29 @@ const NavigationBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#">Entrar</Nav.Link>
-          <Nav.Link href="/register">Registrar</Nav.Link>
-          <NavDropdown
-            title="Produtos"
-            id="basic-nav-dropdown"
-            className="dropdown-menu-right"
-          >
-            <NavDropdown.Item href="#">Colar</NavDropdown.Item>
-            <NavDropdown.Item href="#">Pulseiras</NavDropdown.Item>
-            <NavDropdown.Item href="#">Brincos</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#">Ver todos os produtos</NavDropdown.Item>
-          </NavDropdown>
+          {authenticated ? (
+            <>
+              <NavDropdown
+                title="Produtos"
+                id="basic-nav-dropdown"
+                className="dropdown-menu-right"
+              >
+                <NavDropdown.Item href="#">Colar</NavDropdown.Item>
+                <NavDropdown.Item href="#">Pulseiras</NavDropdown.Item>
+                <NavDropdown.Item href="#">Brincos</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">
+                  Ver todos os produtos
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link onClick={logout}>Sair</Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link href="#">Entrar</Nav.Link>
+              <Nav.Link href="/register">Registrar</Nav.Link>
+            </>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
