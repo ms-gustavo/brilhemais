563e2a60429e44319bf58bbb175d2da60ac34b31
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-23 16:11:05 -0300

    populando pagina de entregas - falta conteudo

--- frontend/src/components/pages/Others/Delivery.js
+++ frontend/src/components/pages/Others/Delivery.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./Others.module.css";
 
 const Delivery = () => {
-  return <div>Delivery</div>;
+  return (
+    <section className={styles.others_section}>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className={`${styles.card_others} card`}>
+            <div className="card-body">
+              <h4 className={`${styles.card_text} card-title`}>Uber Flash</h4>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className={`${styles.card_others} card`}>
+            <div className="card-body">
+              <h4 className={`${styles.card_text} card-title`}>Presencial</h4>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Delivery;
